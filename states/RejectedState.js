af32f25991217557bdbbc21f60c304092d2bc325
Nhựt Tân <119086524+Nguyennhuttan2004@users.noreply.github.com>
2025-08-23 18:10:31 +0700

    commit

---
+++ states/RejectedState.js
@@ -0,0 +1,9 @@
+const OrderState = require("./OrderState");
+
+class RejectedState extends OrderState {
+  setStatus(newState) {
+    throw new Error("Rejected orders cannot change state");
+  }
+}
+module.exports = RejectedState;
+
